67f7002118c270780a849b1c7ec0004710658397
imshubhamkumar <shubhamk773@gmail.com>
2020-03-17 23:59:21 +0530

    card background color fixed

--- server/app.js
+++ server/app.js
@@ -30,9 +30,9 @@ mongoose
 
 
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-//   });
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  });
   
   app.listen(5000, () => {
     console.log('Example app listening on port 5000!')
